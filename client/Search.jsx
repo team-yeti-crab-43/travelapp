acfb22f7e10304161ad52f288413f33e522872e7
Sushanth Kambham <sushanth9@hotmail.com>
2021-05-22 16:00:00 -0700

    Adding react hooks on frontend

--- client/Search.jsx
+++ client/Search.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 const axios = require('axios').default;
 
 // function makeFetchRequestWithSearchData() {
@@ -19,25 +19,41 @@ const axios = require('axios').default;
 //   // call helper function to fetch get request with search input data here
 //   this.makeFetchRequestWithSearchData(); //pass in search bar input data
 // }
+// const handleSubmit = event => {
+//   event.preventDefault();
+//   console.log(event.target.elements.destination);
+// }
 
 const Search = props => {
+
+  const [destination, setDestination] = useState();
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
+  const handleSubmit= (e) => {
+    e.preventDefault();
+    console.log(destination, startDate, endDate);
+  }
+  
+  
   return (
     <div id="search-bar">
       <h3>Where would you like to go?</h3>
-      <form>
-        <input type="text" id="destination" placeholder="destination"></input>
+      <form onSubmit={e => { handleSubmit(e) }}>
+        <input type="text" name="destination" placeholder="destination" onChange={e => setDestination(e.target.value)}/>
         <br></br>
         {/* <button onClick={handleSearchButton}> */}
-        <input type="text" id="start-date" placeholder="start date (MM/DD/YYY)"></input>
-        <input type="text" id="end-date" placeholder="end date (MM/DD/YYY)"></input>
-        <input type="text" id="num-people" placeholder="# of travelers"></input>
+        <input type="date" name="start-date" placeholder="start date (MM/DD/YYY)" onChange={e => setStartDate(e.target.value)}/>
+        <input type="date" name="end-date" placeholder="end date (MM/DD/YYY)" onChange={e => setEndDate(e.target.value)}/>
+        <input type="text" name="num-people" placeholder="# of travelers" />
         <br></br>
-        <button>
+        <button type='submit'>
           Search
         </button>
       </form>
     </div>
     )
-}
-
-export default Search
+  }
+  
+  
+  export default Search
