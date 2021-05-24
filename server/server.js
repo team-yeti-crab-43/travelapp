f7ad851a0331396ae71ea74213638c0db1ccbaa0
Robleh Farah <robleh@Roblehs-MBP.attlocal.net>
2021-05-24 13:44:20 -0700

    Backend with testing and server for hotel in progress

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const path = require('path'); 
+const path = require('path');  
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
@@ -12,6 +12,11 @@ app.use(bodyParser.urlencoded({
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 });
+
+
+
+
+
 
 app.post('/api/test', (req, res) => {
   // console.log(req.body)
@@ -43,36 +48,48 @@ app.post('/api/test', (req, res) => {
       // return res.body;
   })
   // return res.status(200).json('hello');
-}); 
+});
 
 
 
 
+// app.post('/api/test', (req, res) => { //make up another variable on front end, city 
+//   // console.log(req.body)
+//   const {city, startDate, endDate} = req.body  //variables have to be renamed according to what you call them in the request data
+//   console.log('destination :'+ city, 'flightDate: '+startDate, 'returndate : '+endDate)
+//   let request = unirest("GET", "https://hotels-com-provider.p.rapidapi.com/v1/hotels/search?sort_order=PRICE&checkout_date=2021-10-01&locale=en_US&currency=USD&adults_number=1&destination_id=11594&checkin_date=2021-09-01");
 
-// function apiResponse(){
-// let result; 
-
-// var req = unirest("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/LAX-sky/UK/2021-09-01");
-
-// req.query({
-// 	"inboundpartialdate": "2021-12-01"
-// });
-
-// req.headers({
-// 	"x-rapidapi-key": "e15b5e2e5dmshc81eceff76fe168p18f7e3jsn43a131d13978",
-// 	"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-// 	"useQueryString": true
-// });
+//   // "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/LAX-sky/UK/2021-09-01" sample
+//   // "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/LAX-sky/"+airportLocation+"/"+startDate concat
 
 
-// req.end(function (res) {
-// 	if (res.error) throw new Error(res.error);
-// 		console.log(res.body)
-// 	 result = res.body;
-// })
+//   request.query({
+//     "sort_order": "PRICE",
+//     "checkout_date": endDate,
+//     "locale": "en_US",
+//     "currency": "USD",
+//     "adults_number": "1",
+//     "destination_id": "11594", // Dubai London: 549499
+//     "checkin_date": startDate
+//   });
+  
+//   req.headers({
+//     "x-rapidapi-key": "e15b5e2e5dmshc81eceff76fe168p18f7e3jsn43a131d13978",
+//     "x-rapidapi-host": "hotels-com-provider.p.rapidapi.com",
+//     "useQueryString": true
+//   });
 
-// return result; 
+//   request.end(function (response) {
+//     if (response.error) {
+//     // throw new Error(response.error);
+//     return res.status(400)
+//     }
+//       // console.log(response.body.Quotes)
+//       res.status(200).json({info: response.body.Quotes[0].MinPrice});
+//       // return res.body;
+//   })
+//   // return res.status(200).json('hello');
+// }); 
 
-// }
 
 app.listen(3000);
